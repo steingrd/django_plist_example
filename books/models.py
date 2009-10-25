8fcccff2399f53e6e0d9e0f91d0d0c6ce30cb3f9
Steingrim Dovland <steingrd@ifi.uio.no>
2009-10-25 11:45:45 +0100

    added blog-field and __unicode__ for models

--- books/models.py
+++ books/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=128)
-    nationality = models.CharField(max_length=2)
+    blog = models.URLField()
+
+    def __unicode__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=128)
     author = models.ForeignKey(Author)
     price = models.IntegerField()
     released = models.DateField()
+
+    def __unicode__(self):
+        return self.title
